77895be263ee4ba23c3d984abd4403590370ddd2
Michael Cho <michael@michaelcho.dev>
2026-01-27 00:29:57 -0500

    dbcsr-mpi 2.9.1 (new formula)

---
+++ Formula/d/dbcsr-mpi.rb
@@ -0,0 +1,48 @@
+class DbcsrMpi < Formula
+  desc "Distributed Block Compressed Sparse Row matrix library"
+  homepage "https://cp2k.github.io/dbcsr/"
+  url "https://github.com/cp2k/dbcsr/releases/download/v2.9.1/dbcsr-2.9.1.tar.gz"
+  sha256 "fa5a4aeba0a07761511af2c26c779bd811b5ea0ef06a5d94535b6dd7b2e0ce59"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    formula "dbcsr"
+  end
+
+  keg_only "it conflicts with dbcsr"
+
+  depends_on "cmake" => :build
+  depends_on "fypp" => :build
+  depends_on "gcc" # for gfortran
+  depends_on "open-mpi"
+  depends_on "openblas"
+
+  uses_from_macos "python" => :build
+
+  fails_with :clang do
+    cause "needs to be aligned with `cp2k` and `openblas` to avoid mixing OpenMP"
+  end
+
+  def install
+    rm_r("tools/build_utils/fypp")
+
+    args = %w[
+      -DBUILD_SHARED_LIBS=ON
+      -DUSE_MPI=ON
+      -DUSE_MPI_F08=ON
+      -DUSE_SMM=blas
+      -DWITH_EXAMPLES=OFF
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    pkgshare.install "examples/dbcsr_example_3.cpp"
+  end
+
+  test do
+    openmpi = Formula["open-mpi"]
+    system ENV.cxx, pkgshare/"dbcsr_example_3.cpp", "-o", "test", "-fopenmp",
+                    "-L#{lib}", "-ldbcsr_c", "-L#{openmpi.opt_lib}", "-lmpi"
+    system openmpi.bin/"mpirun", "./test"
+  end
+end
